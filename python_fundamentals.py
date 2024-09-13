32fcb5e8bf8ace6076565f2529690c9b1ea3eaa9
Scott Hadzik <scotthadzik@weber.edu>
2024-09-12 19:00:11 -0600

    added info on variables and comments

---
+++ python_fundamentals.py
@@ -0,0 +1,11 @@
+# This is a comment a comment is not executed
+
+# variable is word that stores information
+# loosely type DATA Type
+#     STRING -> chars = Scott  5rFtz
+#     int integer 1 3 4 5678
+#     float decimal number 1.0 12.4 4.5678 
+username = 'Scott'
+a_number = 4 # datatype is int
+a_float = 4.6 # datatype a float
+string_number = '4' # a string datatype
